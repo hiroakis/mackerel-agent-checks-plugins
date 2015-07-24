ec112da111ec160c700be9cb9aca85e9de7da02c
Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
2015-07-24 21:04:41 +0900

    snake case -> camel case

--- mackerel-check-mysql-replication/mackerel-check-mysql-replication.go
+++ mackerel-check-mysql-replication/mackerel-check-mysql-replication.go
@@ -43,24 +43,24 @@ func main() {
 		os.Exit(OK)
 	}
 
-	idx_io_thread_runninng := res.Map("Slave_IO_Running")
-	idx_sql_thread_runninng := res.Map("Slave_SQL_Running")
-	idx_seconds_behind_master := res.Map("Seconds_Behind_Master")
-	io_thead_status := rows[0].Str(idx_io_thread_runninng)
-	sql_thead_status := rows[0].Str(idx_sql_thread_runninng)
-	seconds_behind_master := rows[0].Int(idx_seconds_behind_master)
+	idxIoThreadRunning := res.Map("Slave_IO_Running")
+	idxSQLThreadRunning := res.Map("Slave_SQL_Running")
+	idxSecondsBehindMaster := res.Map("Seconds_Behind_Master")
+	ioThreadStatus := rows[0].Str(idxIoThreadRunning)
+	sqlThreadStatus := rows[0].Str(idxSQLThreadRunning)
+	secondsBehindMaster := rows[0].Int(idxSecondsBehindMaster)
 
-	if io_thead_status == "No" || sql_thead_status == "No" {
+	if ioThreadStatus == "No" || sqlThreadStatus == "No" {
 		fmt.Println("CRITICAL: MySQL replication has been stopped")
 		os.Exit(CRITICAL)
 	}
 
-	if seconds_behind_master > *optCrit {
-		msg := fmt.Sprintf("CRITICAL: MySQL replication behind master %d seconds", seconds_behind_master)
+	if secondsBehindMaster > *optCrit {
+		msg := fmt.Sprintf("CRITICAL: MySQL replication behind master %d seconds", secondsBehindMaster)
 		fmt.Println(msg)
 		os.Exit(CRITICAL)
-	} else if seconds_behind_master > *optWarn {
-		msg := fmt.Sprintf("WARNING: MySQL replication behind master %d seconds", seconds_behind_master)
+	} else if secondsBehindMaster > *optWarn {
+		msg := fmt.Sprintf("WARNING: MySQL replication behind master %d seconds", secondsBehindMaster)
 		fmt.Println(msg)
 		os.Exit(WARNING)
 	}
